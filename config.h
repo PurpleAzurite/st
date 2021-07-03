9203442b72ea39b5f8f94d1501e95727617a54d8
purpleazurite <misagh.asgari@gmail.com>
2021-07-03 19:54:17 +0430

    Update some keybindings

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
-/* See LICENSE file for copyright and license details. */
+/* See Lcopyright and license details. */
 
 /*
  * appearance
@@ -205,10 +205,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	/* { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} }, */
-	/* { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} }, */
-	{ ShiftMask,            XK_I,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_P,   kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
