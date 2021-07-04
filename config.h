0e89119b1224c3b2f67d3a6a7e6e5e61c9d62014
purpleazurite <misagh.asgari@gmail.com>
2021-07-04 08:15:57 +0430

    Add pywal-generated colorscheme support. This can be disabled by removing pywal include file, uncommenting the array, and uncommenting the default color definitions. Reduced opacity by 0.1 to 0.9.

--- config.h
+++ config.h
@@ -94,47 +94,48 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
+#include "/home/david/.cache/wal/colors-wal-st.h"
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+/* static const char *colorname[] = { */
+/* 	/\* 8 normal colors *\/ */
+/* 	"black", */
+/* 	"red3", */
+/* 	"green3", */
+/* 	"yellow3", */
+/* 	"blue2", */
+/* 	"magenta3", */
+/* 	"cyan3", */
+/* 	"gray90", */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+/* 	/\* 8 bright colors *\/ */
+/* 	"gray50", */
+/* 	"red", */
+/* 	"green", */
+/* 	"yellow", */
+/* 	"#5c5cff", */
+/* 	"magenta", */
+/* 	"cyan", */
+/* 	"white", */
 
-	[255] = 0,
+/* 	[255] = 0, */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
-};
+/* 	/\* more colors can be added after 255 to use with DefaultXX *\/ */
+/* 	"#cccccc", */
+/* 	"#555555", */
+/* 	"black", */
+/* }; */
 
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+/* unsigned int defaultfg = 7; */
+/* unsigned int defaultbg = 258; */
+/* static unsigned int defaultcs = 256; */
+/* static unsigned int defaultrcs = 257; */
 
 /*
  * Default shape of cursor
