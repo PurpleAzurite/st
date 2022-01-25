fd1a7ac711dfcc8119d9f69b3bcae86396f1715a
PurpleAzurite <misagh.asgari@gmail.com>
2022-01-25 20:41:48 +0300

    St should no longer crash when faced with emojis
    - "Suckless software..."

--- config.h
+++ config.h
@@ -6,6 +6,9 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrains Mono:pixelsize=19:antialias=true:autohint=true";
+static char *font1 = {
+	"Hack Nerd Font:pixelsize=19:antialias=true:autohint=true",
+};
 static int borderpx = 2;
 
 /*
