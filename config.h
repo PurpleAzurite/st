561d0757e7cb0ccd325b3cd0a33971692d045eee
PurpleAzurite <misagh.asgari@gmail.com>
2022-01-28 15:51:02 +0300

    Revert "St should no longer crash when faced with emojis"
    This reverts commit fd1a7ac711dfcc8119d9f69b3bcae86396f1715a.

--- config.h
+++ config.h
@@ -6,9 +6,6 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrains Mono:pixelsize=19:antialias=true:autohint=true";
-static char *font1 = {
-	"Hack Nerd Font:pixelsize=19:antialias=true:autohint=true",
-};
 static int borderpx = 2;
 
 /*
