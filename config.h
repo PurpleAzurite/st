513b561386f9ce9492295ef87c5bcbc36d042538
PurpleAzurite <misagh.asgari@gmail.com>
2022-02-01 14:57:59 +0300

    config.def.h is no longer supported

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 1.0;
 
 #include "/home/david/.cache/wal/colors-wal-st.h"
 /* Terminal colors (16 first used in escape sequence) */
