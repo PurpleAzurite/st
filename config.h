c9e72fbb2b6907e66de8ac032f9a61a44415bff7
Misagh Asgari <misagh.asgari@gmail.com>
2022-07-22 18:54:24 +0300

    Soda!

--- config.h
+++ config.h
@@ -96,7 +96,7 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 float alpha = 0.9;
 
-#include "/home/david/.cache/wal/colors-wal-st.h"
+#include "/home/soda/.cache/wal/colors-wal-st.h"
 /* Terminal colors (16 first used in escape sequence) */
 /* static const char *colorname[] = { */
 /* 	/\* 8 normal colors *\/ */
